187ac3d8e349d8bde5eb4f495bcafa315b5a493a
peterjc <p.j.a.cock@googlemail.com>
2014-01-23 16:41:46 +0000

    Commit handle version of script

--- writing_sequence_files/length_filter.py
+++ writing_sequence_files/length_filter.py
@@ -3,8 +3,10 @@ input_filename = "NC_000913.faa"
 output_filename = "NC_000913_long_only.faa"
 count = 0
 total = 0
+output_handle = open(output_filename, "w")
 for record in SeqIO.parse(input_filename, "fasta"):
     total = total + 1
     if 100 <= len(record):
-        count = count + SeqIO.write(record, output_filename, "fasta")
+        count = count + SeqIO.write(record, output_handle, "fasta")
+output_handle.close()
 print(str(count) + " records selected out of " + str(total))
