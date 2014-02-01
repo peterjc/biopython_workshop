ede12becb0dc5dbed53f4921d5562f20a1138f15
Peter Cock <p.j.a.cock@googlemail.com>
2014-02-01 16:29:15 +0000

    Polishing

---
+++ reading_writing_alignments/count_gaps.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from Bio import AlignIO
+
+filename = "PF08792_seed.sth"
+alignment = AlignIO.read("PF08792_seed.sth", "stockholm")
+gaps = 0
+for record in alignment:
+    gaps = gaps + record.seq.count("-")
+count = len(alignment) # number of records
+print(filename + " had " + str(count) + " records,")
+print("Total gaps " + str(gaps) + ", average per record " + str(gaps / count))
