62c0c1da446283e51f91b6ae276a3b5431eb1551
Peter Cock <p.j.a.cock@googlemail.com>
2015-03-14 13:14:03 +0000

    Hackery to get fetch_sample_data.py working on Mac

--- tests/fetch_sample_data.py
+++ tests/fetch_sample_data.py
@@ -26,6 +26,8 @@ else:
     sys.exit(1)
 
 
+use_curl = (sys.platform == "darwin")
+
 print("Extracting commands from %s to fetch sample data..." % sample_data)
 commands = []
 with open(sample_data) as handle:
@@ -33,11 +35,34 @@ with open(sample_data) as handle:
         if line.startswith("    $ "):
             line = line[5:].strip()
             if line.startswith("curl "):
-                # Mac OS X variant, ignore
-                continue
+                if use_curl:
+                    # Use this in preference to previous entry,
+                    commands.pop()
+                else:
+                    # Ignore this
+                    continue
             commands.append(line)
 
+def wget_to_curl(cmd):
+    if not cmd.startswith("wget "):
+        return cmd
+    if cmd.startswith(("wget http", "wget ftp")):
+        return "curl -O " + cmd[5:]
+    elif cmd.startswith("wget -O "):
+        return "curl -o " + cmd[8:]
+    else:
+        # Currently hard coding tricky wget/curl cases
+        # directly in the input RST file...
+        raise NotImplementedError(cmd)
+assert wget_to_curl('wget http://example.org/data.ext') == \
+    'curl -O http://example.org/data.ext'
+assert wget_to_curl('wget -O "example.ext" http://example.org/data?arg=value"') == \
+    'curl -o "example.ext" http://example.org/data?arg=value"'
+
+
 for cmd in commands:
+    if use_curl:
+        cmd = wget_to_curl(cmd)
     print(cmd)
     return_code = os.system(cmd)
     if return_code:
