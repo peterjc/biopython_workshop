d95478d78b85123a6b728b31daf465092ca89f90
peterjc <p.j.a.cock@googlemail.com>
2015-03-16 10:15:22 +0000

    Bash if statements to support Linux and Mac OS X

--- fetch_sample_data.sh
+++ fetch_sample_data.sh
@@ -2,26 +2,46 @@
 # Set bash strict mode (fail on errors, undefined variables, and via pipes)
 set -euo pipefail
 
+if [ -x "$(command -v wget)" ]; then
+  # e.g. Linux
+  echo "Downloading files using wget"
+  FETCH="wget"
+elif [ -x "$(command -v curl)" ]; then
+  # e.g. Max OS X
+  echo "Downloading files using curl"
+  FETCH="curl -O"
+else
+  echo "ERROR: Failed to find wget or curl"
+  exit 1
+fi
+
 echo "=============================================="
 echo "Fetching Escherichia coli K-12 files from NCBI"
 echo "=============================================="
 
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.gbk
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.fna
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.ffn
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.faa
+$FETCH ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.gbk
+$FETCH ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.fna
+$FETCH ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.ffn
+$FETCH ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/NC_000913.faa
 
 echo "=========================================================="
 echo "Fetching proteins from Potato Genome Sequencing Consortium"
 echo "=========================================================="
 
-wget http://potato.plantbiology.msu.edu/data/PGSC_DM_v3.4_pep_representative.fasta.zip
+$FETCH http://potato.plantbiology.msu.edu/data/PGSC_DM_v3.4_pep_representative.fasta.zip
 unzip PGSC_DM_v3.4_pep_representative.fasta.zip
 
 echo "===================================="
 echo "Fetching PF08792 alignment from PFAM"
 echo "===================================="
 
-wget -O "PF08792_seed.sth" http://pfam.sanger.ac.uk/family/PF08792/alignment/seed/format?format=stockholm
-# Note: Mac OS alternative needs -L due to link redirect:
-# curl -o "PF08792_seed.sth" -L http://pfam.sanger.ac.uk/family/PF08792/alignment/seed/format?format=stockholm
+if [ -x "$(command -v wget)" ]; then
+  # Note: Using -O to set the filename explicitly as default is format?format=stockholm
+  wget -O "PF08792_seed.sth" http://pfam.sanger.ac.uk/family/PF08792/alignment/seed/format?format=stockholm
+elif [ -x "$(command -v curl)" ]; then
+  # Note: Mac OS alternative needs -L due to link redirect:
+  curl -o "PF08792_seed.sth" -L http://pfam.sanger.ac.uk/family/PF08792/alignment/seed/format?format=stockholm
+else
+  echo "ERROR: Failed to find wget or curl"
+  exit 1
+fi
